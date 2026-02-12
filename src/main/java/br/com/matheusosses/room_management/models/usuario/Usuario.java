85717f90f5c0641c092e9cb9893a08d2625f2079
matheusosses <maths0ss3s@gmail.com>
2026-02-12 14:27:05 -0300

    feat: entidade usuario

---
+++ src/main/java/br/com/matheusosses/room_management/models/usuario/Usuario.java
@@ -0,0 +1,17 @@
+package br.com.matheusosses.room_management.models.usuario;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+}
